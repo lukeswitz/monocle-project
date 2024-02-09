cef3ce9f50ac902035e5111b95e9d49b54aef404
Luke Switzer <luke@rootdowndigital.com>
2024-02-09 16:10:50 -0700

    update tutorial

--- ios/Noa/Noa/Views/Tutorial.swift
+++ ios/Noa/Noa/Views/Tutorial.swift
@@ -9,11 +9,11 @@ import UIKit
 
 func displayTutorialInChatWindow(chatMessageStore: ChatMessageStore) async throws {
     let messages: [(pause: Float, image: UIImage?, text: String)] = [
-        ( pause: 2, image: nil, text: "Hi, I'm ThirdEye. Let's show you around 🙂" ),
-        ( pause: 5, image: UIImage(named: "Tutorial_2"), text: "Tap either of the touch pads and speak.\n\nAsk me any question, and I'll respond directly on your Monocle." ),
-        ( pause: 5, image: UIImage(named: "Tutorial_3"), text: "I can also translate whatever I hear into English.\n\nToggle the translator mode from the menu like so." ),
+        ( pause: 2, image: nil, text: "Hi I'm a cyborg eyepiece, Let's show you around 🙂" ),
+        ( pause: 3, image: UIImage(named: "Tutorial_2"), text: "Tap either of the touch pads and speak. I'll respond directly on your Monocle." ),
+        ( pause: 3, image: UIImage(named:"Tutorial_3_alt"), text: "I can also translate whatever I hear into English, or .\n\nUse the gear icon at the top right to switch modes or enter your OpenAI API key." ),
 //        ( pause: 5, image: UIImage(named: "Tutorial_4"), text: "Did you know that I'm a fantastic artist? Tap then hold, and Monocle will take a picture before listening.\n\nAsk me how to change the image, and I'll return back a new image right here in the chat." ),
-        ( pause: 0, image: nil, text: "Looks like you're all set!\n\nGo ahead. Ask me anything you'd like ☺️" )
+        ( pause: 0, image: nil, text: "Go ahead. Ask me anything" )
     ]
 
     for (pause, image, text) in messages {
